9dec9b37e9a44d47b36f6b150c88c4d7d3db360e
LexyRoses333 <lexyroses@gmail.com>
2026-06-16 23:38:30 -0700

    First elements of World of Color spawned in

--- Playground.ts
+++ Playground.ts
@@ -7,10 +7,10 @@ import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
 
 registerStart(start);
 async function start() {
-  playgroundDemos.spawnPaintableSphere(new Vector3(-1.5, 2, -3));
-  playgroundDemos.canvas(new Vector3(0, 0.85, -4), Quaternion.one, Vector3.one);
-  lexy.spawnDrawSettingButtons(new Vector3(0, 2.05, -4.15));
-  playgroundDemos.colorPicker(new Vector3(1, 1.5, -3.5), Quaternion.fromEuler(new Vector3(0, -Math.PI / 4, 0)), new Vector3(0.35, 1, 0.35));
+  // playgroundDemos.spawnPaintableSphere(new Vector3(-1.5, 2, -3));
+  // playgroundDemos.canvas(new Vector3(0, 0.85, -4), Quaternion.one, Vector3.one);
+  // lexy.spawnDrawSettingButtons(new Vector3(0, 2.05, -4.15));
+  // playgroundDemos.colorPicker(new Vector3(1, 1.5, -3.5), Quaternion.fromEuler(new Vector3(0, -Math.PI / 4, 0)), new Vector3(0.35, 1, 0.35));
 
   // Come back to this to get water shader working
   // playgroundDemos.spawnShaderSphere(new Vector3(-5, 2.5, -4));
